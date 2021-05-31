6f8d3978bf733d8ba4f96dc4f3967cc495138451
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-05-31 16:01:19 +0200

    esercizio 2 + bonus

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
-// ESERCIZIO 1
+// // ESERCIZIO 1
 
-var emailUtente = prompt('Inserisci la tua email');
-var lista = ['ciao@gmail.com', 'bella@gmail.com', 'sole@gmail.com', 'fra@gmail.com', 'buba@gmail.com'];
-var esitoRicerca = "Email non esistente";
+// var emailUtente = prompt('Inserisci la tua email');
+// var lista = ['ciao@gmail.com', 'bella@gmail.com', 'sole@gmail.com', 'fra@gmail.com', 'buba@gmail.com'];
+// var esitoRicerca = "Email non esistente";
 
-for (var i = 0; i < lista.length; i++) {
-    if (lista[i] == emailUtente) {
-        esitoRicerca = "Email esistente";
-    }    
+// for (let i = 0; i < lista.length; i++) {
+//     if (lista[i] == emailUtente) {
+//         esitoRicerca = "Email esistente";
+//     }    
+// }
+
+// document.getElementById('risultato').innerHTML = esitoRicerca;
+
+
+
+// ESERCIZIO 2
+
+var listaCognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+cognomeUtente = prompt('Inserisci il tuo cognome');
+cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
+listaCognomi.push(cognomeUtente);
+console.log(listaCognomi);
+
+for (let i = 0; i < listaCognomi.length; i++) {
+    listaCognomi[i] = listaCognomi[i].charAt(0).toUpperCase() + listaCognomi[i].slice(1);
 }
 
-document.getElementById('risultato').innerHTML = esitoRicerca;
+listaCognomi.sort();
+console.log(listaCognomi);
+
+for (let i = 0; i < listaCognomi.length; i++) {
+    if (listaCognomi[i] == cognomeUtente) {
+        console.log('La posizione del tuo cognome nella lista ordinata é ', i+1);
+    }
+}
